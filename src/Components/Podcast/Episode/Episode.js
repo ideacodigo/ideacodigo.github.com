690d75a391b426208fade690c65650b7b7719b27
Diego <diegoat214@gmail.com>
2024-11-25 12:19:35 -0500

    WEEK 47

--- src/Components/Podcast/Episode/Episode.js
+++ src/Components/Podcast/Episode/Episode.js
@@ -33,43 +33,70 @@ export const Episode = () => {
               />
             </div>
           </div>
-
         </div>
 
-        
-        <div className='container-fluid text-nowrap fs-7 animate__animated animate__slideOutRight animate__slower'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat temporibus, quia sed accusamus animi inventore eveniet commodi! Beatae necessitatibus dicta sed? Deleniti saepe repellendus doloremque labore sequi ipsa nulla non!
-        </div>
 
         <div className='container-fluid bg-secondary my-4 episode-banner-of-content'>
-          <div className='d-flex justify-content-center pt-3'>
-            <div className='mx-3 text-center'>
-              <div>
-                <img src="./assets/icons/perfil-a.png" alt='...' width="75px" />
+
+          <div className='position-relative'>
+            <div className='text-nowrap fs-7 position-relative'>
+              <div className='position-absolute episode-comments'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat temporibus, quia sed accusamus animi inventore eveniet commodi! Beatae necessitatibus dicta sed? Deleniti saepe repellendus doloremque labore sequi ipsa nulla non!
               </div>
-              <p className='fs-7'><a href='.'>@andreaes</a></p>
             </div>
-            <div className='mx-3 text-center'>
-              <div>
-                <img src="./assets/icons/perfil-b.png" width="75px" alt='...' />
+            <div className='position-absolute w-100 top-50 start-0'>
+
+              <div className='d-flex justify-content-center pt-3'>
+
+                <div className='inline-block mx-3 text-center'>
+                  <div>
+                    <img src="./assets/icons/perfil-a.png" alt='...' width="50px" />
+                  </div>
+                  <p className='fs-7'><a href='.'>@andreaes</a></p>
+                </div>
+                <div className='inline-block mx-3 text-center'>
+                  <div>
+                    <img src="./assets/icons/perfil-b.png" width="50px" alt='...' />
+                  </div>
+                  <p className='fs-7'><a href='.'>@carloso</a></p>
+                </div>
+                <div className='inline-block mx-3 text-center'>
+                  <div>
+                    <img src="./assets/icons/perfil-c.png" width="50px" alt='...' />
+                  </div>
+                  <p className='fs-7'><a href='.'>@lafina</a></p>
+                </div>
+                <div className='inline-block mx-3 text-center'>
+                  <div>
+                    <img src="./assets/icons/perfil-d.png" width="50px" alt='...' />
+                  </div>
+                  <p className='fs-7'><a href='.'>@pasiente</a></p>
+                </div>
               </div>
-              <p className='fs-7'><a href='.'>@carloso</a></p>
             </div>
-            <div className='mx-3 text-center'>
-              <div>
-                <img src="./assets/icons/perfil-c.png" width="75px" alt='...' />
+
+            <div className='w-100 position-absolute episode-form text-center'>
+              <div className='d-flex justify-content-center'>
+                <input
+                  className="form-control-sm form-control mb-3 w-50"
+                  name="email"
+                  value="..."
+                  id="contact_email"
+                  type="email" required />
               </div>
-              <p className='fs-7'><a href='.'>@lafina</a></p>
-            </div>
-            <div className='mx-3 text-center'>
-              <div>
-                <img src="./assets/icons/perfil-d.png" width="75px" alt='...' />
-              </div>
-              <p className='fs-7'><a href='.'>@pasiente</a></p>
+
             </div>
 
           </div>
+
+
+
         </div>
+
+        <div className='container'>
+          <div className='bg-on-secondary episode-top-edge'>{/* line top edge: part top of the casette */}</div>
+        </div>
+
       </div>
     </>
   )
